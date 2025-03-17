66e7880c8f47711d3b7f6448220e188032267f7b
Kizzuato <kyyajuara@gmail.com>
2025-03-17 21:46:35 +0700

    adding show google sheet funct

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,22 +4,27 @@ import "./globals.css";
 import { SidebarProvider } from "@/context/SidebarContext";
 import { ThemeProvider } from "@/context/ThemeContext";
 
+import { getSheetData } from "@/_lib/readSheet";
 const outfit = Outfit({
   variable: "--font-outfit-sans",
   subsets: ["latin"],
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+    const data = await getSheetData()
+    console.log("data: ", data)
   return (
     <html lang="en">
       <body className={`${outfit.variable} dark:bg-gray-900`}>
-        <ThemeProvider>
+        yo 
+        {/* <ThemeProvider>
           <SidebarProvider>{children}</SidebarProvider>
-        </ThemeProvider>
+        </ThemeProvider> */}
       </body>
     </html>
   );
