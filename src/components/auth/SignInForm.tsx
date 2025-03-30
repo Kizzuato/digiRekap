3aa47beec03e47019ed3f4a61047e600c2ccecb3
Kizzuato <kyyajuara@gmail.com>
2025-03-30 17:28:41 +0700

    fixing bug on rekap keuangan data

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -116,9 +116,9 @@ export default function SignInForm() {
                   </Link>
                 </div>
                 <div>
-                  <Button className="w-full" size="sm">
+                <Link href="/dashboard" className="flex items-center justify-center w-full px-4 py-3 text-sm font-medium text-white transition rounded-lg bg-brand-500 shadow-theme-xs hover:bg-brand-600">
                     Masuk
-                  </Button>
+                  </Link>
                 </div>
               </div>
             </form>
